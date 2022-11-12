1de01724996215dedf7a8fd2e7a66995e29f5d95
hoangtoan <hoangtoan04102001@gmail.com>
2022-11-12 10:35:56 +0700

    push project

---
+++ src/main/java/com/pttkht/nhom7/Nhom7Application.java
@@ -0,0 +1,13 @@
+package com.pttkht.nhom7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nhom7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nhom7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pttkht/nhom7/Nhom7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pttkht.nhom7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nhom7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
